5eb8adb9918cc6361747556854bced6917107f0d
chjcmy <chjcmy@naver.com>
2020-08-05 16:55:45 +0900

    js

--- baekjoon/src/Main.java
+++ baekjoon/src/Main.java
@@ -1,31 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bf.readLine());
-        int[][] dp = new int[N][3];
+        int n = Integer.parseInt(bf.readLine());
 
-        for(int i=0; i<N; i++) {
-            StringTokenizer st = new StringTokenizer(bf.readLine());
-            dp[i][0] = Integer.parseInt(st.nextToken());
-            dp[i][1] = Integer.parseInt(st.nextToken());
-            dp[i][2] = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[n][n];
+
+        StringTokenizer tokenizer;
+        for(int i=0 ; i<n ; i++){
+            tokenizer = new StringTokenizer(bf.readLine());
+            for(int j=0 ; j<i+1 ; j++){
+                dp[i][j] = Integer.parseInt(tokenizer.nextToken());
+            }
         }
 
-        for(int i=1; i<N; i++) {
-            dp[i][0] += Math.min(dp[i-1][1], dp[i-1][2]);
-            dp[i][1] += Math.min(dp[i-1][0], dp[i-1][2]);
-            dp[i][2] += Math.min(dp[i-1][0], dp[i-1][1]);
+        for(int i=1 ; i<n ; i++){
+            for(int j=0 ; j<i+1 ; j++){
+                dp[i][j] += Math.max((j==0) ? 0 : dp[i-1][j-1], dp[i-1][j]);
+            }
         }
-        int a = Math.min(dp[N-1][0], (Math.min(dp[N-1][1], dp[N-1][2])));
-        System.out.println(a);
+
+        Arrays.sort(dp[n-1]);
+        System.out.println(dp[n-1][n-1]);
     }
 
 }
-
-
